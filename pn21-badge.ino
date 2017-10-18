f205adf24a641a8b764d9f06c6be350299207ae9
Hunter Fuller <hfuller@pixilic.com>
2017-10-18 13:09:42 -0500

    Add sleep helper function
    Keep us awake while plugged into a pc. otherwise low power sleep.

--- pn21-badge.ino
+++ pn21-badge.ino
@@ -1,9 +1,19 @@
 #include <Adafruit_CircuitPlayground.h>
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_SleepyDog.h>
 
 #define NUM_LEDS 10
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_LEDS, 8, NEO_GRB + NEO_KHZ800);
+
+void sleep(unsigned long ms) {
+	if ( millis() < 10000 || Serial ) {
+		//Either we haven't been running for long, or we are connected to a PC.
+		delay(ms);
+	} else {
+		Watchdog.sleep(ms);
+	}
+}
 
 void setup() {
 	pinMode(13, OUTPUT);
@@ -12,9 +22,9 @@ void setup() {
 
 void loop() {
 	digitalWrite(13, HIGH);
-	pixels.setPixelColor(0, pixels.Color(255,0,0));
-	pixels.show();
-	delay(1000);
+	sleep(75);
+	digitalWrite(13, LOW);
+	sleep(1925);
 							
 	digitalWrite(13, LOW);		
 	pixels.setPixelColor(0, pixels.Color(0,0,0));
